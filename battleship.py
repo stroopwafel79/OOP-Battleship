af1e6f9c6793eb5d9aea22e70d0b85060efe4354
stroopwafel79 <stroopwafel79@gmail.com>
2019-02-12 23:42:32 +0000

    Added pseudocode

--- battleship.py
+++ battleship.py
@@ -1,9 +1,17 @@
 # Need:
 # A board
+  Attributes:
+    in_play = true
+  Actions:
+  	create board
+  	display board
+  	check for hits
+  	check for wins
+
 # Ships
 	Need to know:
 		num holes - 5 holes, 3 holes, 3 holes, 2 holes
 		orientation - horizontal or vertical
 # Players
 	player_1 - create a board
-	player_2
+	player_2 - create a board
