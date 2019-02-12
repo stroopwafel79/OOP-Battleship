dc71eb4e26231aac885e4e096a84a134d9af9058
stroopwafel79 <stroopwafel79@gmail.com>
2019-02-12 23:40:19 +0000

    Initial Commit

---
+++ battleship.py
@@ -0,0 +1,9 @@
+# Need:
+# A board
+# Ships
+	Need to know:
+		num holes - 5 holes, 3 holes, 3 holes, 2 holes
+		orientation - horizontal or vertical
+# Players
+	player_1 - create a board
+	player_2
